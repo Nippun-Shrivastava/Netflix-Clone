cf94de4d4f98e32ef6e018370f283db201c89682
Nippun Shrivastava <135109988+Nippun-Shrivastava@users.noreply.github.com>
2024-09-29 22:32:16 +0530

    third

--- Netflix-clone/src/Components/Header.jsx
+++ Netflix-clone/src/Components/Header.jsx
@@ -2,7 +2,7 @@ import NetflixLogo from "../assets/NetflixLogo.svg";
 import React from "react";
 function Header() {
   return (
-    <div className="w-90% h-[100px] bg-black">
+    <div className="w-80% h-[100px] bg-black">
       {/* <img src={NetflixLogo} alt="netflix-logo" className="w-[30px]" /> */}
     </div>
   );
