916678db6391266c5137359d5c74ddbe6406778b
Nippun Shrivastava <135109988+Nippun-Shrivastava@users.noreply.github.com>
2024-09-29 23:23:15 +0530

    Tailwind error solved

--- Netflix-clone/src/Components/Header.jsx
+++ Netflix-clone/src/Components/Header.jsx
@@ -2,8 +2,8 @@ import NetflixLogo from "../assets/NetflixLogo.svg";
 import React from "react";
 function Header() {
   return (
-    <div className="w-80% h-[100px] bg-black">
-      {/* <img src={NetflixLogo} alt="netflix-logo" className="w-[30px]" /> */}
+    <div>
+      <img src={NetflixLogo} alt="netflix-logo" className="w-30px" />
     </div>
   );
 }
