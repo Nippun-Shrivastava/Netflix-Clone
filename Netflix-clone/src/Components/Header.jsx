cdcf420ea3c4d70a3a991cb1ecef9278a6b2ac39
Nippun Shrivastava <135109988+Nippun-Shrivastava@users.noreply.github.com>
2024-10-04 00:30:57 +0530

    logo & button

--- Netflix-clone/src/Components/Header.jsx
+++ Netflix-clone/src/Components/Header.jsx
@@ -1,9 +1,18 @@
-import NetflixLogo from "../assets/NetflixLogo.svg";
+import NetflixLogo from "../assets/Netflix.png";
 import React from "react";
+
 function Header() {
   return (
-    <div>
-      <img src={NetflixLogo} alt="netflix-logo" className="w-[300px]" />
+    <div className="border border-black pl-24 px-4 bg-gradient-to-b from-black h-14 w-[100%] flex justify-between items-center">
+      <img
+        src={NetflixLogo}
+        alt="netflix-logo"
+        className="w-[130px] object-contain"
+      />
+      <div className="flex justify-between items-center space-x-4 pr-4">
+        <button className="text-white">Logout</button>
+        <button className="text-white">Search Movies</button>
+      </div>
     </div>
   );
 }
