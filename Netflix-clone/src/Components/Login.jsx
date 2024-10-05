e5a81cc7f86342f467bf7f631001930c54e88752
Nippun Shrivastava <135109988+Nippun-Shrivastava@users.noreply.github.com>
2024-10-06 00:20:48 +0530

    login page

--- Netflix-clone/src/Components/Login.jsx
+++ Netflix-clone/src/Components/Login.jsx
@@ -8,7 +8,7 @@ function Login() {
       <div className="absolute">
         <img src={Background} alt="login-page-background" />
       </div>
-      <form className="absolute w-3/12 flex flex-col my-36 left-0 right-0 mx-auto items-center justify-center p-12 bg-black">
+      <form className="absolute w-3/12 flex flex-col my-36 left-0 right-0 mx-auto items-center justify-center p-12 bg-black opacity-80">
         <div className="flex flex-col justify-center items-center w-full">
           <h1 className="text-left mb-4">Login</h1>
           <input
